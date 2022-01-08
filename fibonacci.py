75656a3fe71d1570e355c29ba9c5a52583f49897
anupART <94508205+anupART@users.noreply.github.com>
2022-01-08 14:07:50 +0530

    Create fibonacci.py
    this is program for fibonnaic series

---
+++ fibonacci.py
@@ -0,0 +1,19 @@
+
+n=int(input("Enter a number upto you want to  find Fibonaic numbers : "))
+f=0
+s=1
+i=0
+# i is initialize
+
+# f=first term that is 0
+# s=second term that is 1
+# sum=0
+print("The Fibonaic series is :- ")
+while (i<=n):
+    # conitioning
+
+    print(i)
+    f=s
+    s=i
+    i=f+s
+    
