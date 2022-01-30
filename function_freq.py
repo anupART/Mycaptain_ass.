76ce9f1c30c6e7eb9f1f35d930baa7a1ccfe217f
anupART <94508205+anupART@users.noreply.github.com>
2022-01-30 15:22:09 +0530

    Add files via upload
    resubmittion of frequency q

--- function_freq.py
+++ function_freq.py
@@ -1,3 +1,6 @@
+
+
+
 str=input("Enter a string :")
 print("string is ",str)
 count={}
@@ -9,5 +12,30 @@ for x in str:
         # print(x,"=",count[x])
 print(count)
 
-for x in  count.keys():
-     print(x,"=",count[x])
+
+
+# for x in  Counter.keys():
+#  for x in  reversed(count.keys()):
+#       print(x,"=",count[x])
+
+# text = input("Enter the string : ")
+# count = { }
+# for ch in text:
+#     if ch in count:
+#         count[ch] += 1
+#     else:
+#         count[ch] = 1
+# print(count)
+
+
+def function_freq(string):
+    d={}
+    for i in string:
+        if i in d:
+            d[i]+=1
+        else:
+            d[i]=1
+    return d
+
+str=input("Enter the String :")
+print(function_freq(str))
